ce26a124cc966181bf15771edf476eadbc2601d2
GaganUppaldev <149155501+GaganUppaldev@users.noreply.github.com>
2024-08-01 22:28:33 +0530

    Create conditions.cpp
    conditions in c++ and taking input from user , using if else statement is also important

---
+++ conditions.cpp
@@ -0,0 +1,21 @@
+//input we get using cin>>
+//if else
+//using cin.get() we can take space , tab and enter as input i have not tried below But imp to read thing
+#include<iostream>
+
+using namespace std;
+int main(){
+  int a ;
+  cin>>a; //take input from user
+  cout<<"user input is  "<<a<<endl;
+
+  if(a>0){
+    cout<<"a is positive"<<endl;
+  }
+  else{
+    cout<<"a is negative"<<endl;
+  }
+
+
+  
+}
