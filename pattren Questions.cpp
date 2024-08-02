489dc6a1aa172ffff2989feaf681cfc84aa436f8
GaganUppaldev <149155501+GaganUppaldev@users.noreply.github.com>
2024-08-03 01:30:58 +0530

    Update pattren Questions.cpp
    Added a question that i solved myself and invested time in thinking rather than watching tutorial , AI or google . Question 4 is really a memory and confidence maker

--- pattren Questions.cpp
+++ pattren Questions.cpp
@@ -43,7 +43,7 @@ int main(){
 
 }
 
-  //<QUESTION2>
+  //<QUESTION2>-----------------------------------------------------------------------------------------------------------------------
 
   /*
   Write a program to print the following pattern:
@@ -101,6 +101,59 @@ int main(){
  
 }
 */
+/*======================================================================================================================================
+<Question 4> 
+actually i solve sum questions before it
+
+QUESTION IS TO GET PATTREN LIKE BELOW ON GIVING INPUT = 5 ( triangle pattren)
+*
+**
+***
+****
+*****
+******
+
+
+//for loop
+#include <iostream>
+using namespace std;
+int main(){
+  int a ;
+  for (a=1;a<=5;a++){
+    cout<<a<<endl;
+  }
+  int b;
+  int n;
+  cin>>n;
+  cout<<n<<" is your output"<<endl;
+  for(b=1;b<=n;b++){
+    cout<<b<<" ";
+  }
+
+  int d;
+  cin>>d;
+  cout<<d<<" is your output"<<endl;
+
+  int sum = 0;
+
+  for (int i = 1 ; i<=d;i++){ 
+    
+    sum = sum+i;// 0+1 = 1 , 1 + 2 + 3, 3+3 = 6 ,6+4 =10 ,10+5 =15
+    
+    
+  }
+  cout<<sum<<endl;//1
+
+ 
+
+  
+
+  
+}
+
+
+*/
+
   
 
   
