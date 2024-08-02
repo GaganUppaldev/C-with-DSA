c8477271c728bb03bd691e76cab8a168ea8e685e
GaganUppaldev <149155501+GaganUppaldev@users.noreply.github.com>
2024-08-03 01:35:00 +0530

    Update pattren Questions.cpp
    I pasted code which was by default on my clipboard so i now pasted my own code and changed a code gave space in between stars
    * 
    * * 
    * * * 
    * * * * 
    * * * * * 
    * * * * * *

--- pattren Questions.cpp
+++ pattren Questions.cpp
@@ -106,48 +106,31 @@ int main(){
 actually i solve sum questions before it
 
 QUESTION IS TO GET PATTREN LIKE BELOW ON GIVING INPUT = 5 ( triangle pattren)
-*
-**
-***
-****
-*****
-******
+* 
+* * 
+* * * 
+* * * * 
+* * * * * 
+* * * * * * 
 
 
-//for loop
 #include <iostream>
 using namespace std;
 int main(){
-  int a ;
-  for (a=1;a<=5;a++){
-    cout<<a<<endl;
-  }
-  int b;
   int n;
   cin>>n;
-  cout<<n<<" is your output"<<endl;
-  for(b=1;b<=n;b++){
-    cout<<b<<" ";
-  }
-
-  int d;
-  cin>>d;
-  cout<<d<<" is your output"<<endl;
-
-  int sum = 0;
-
-  for (int i = 1 ; i<=d;i++){ 
-    
-    sum = sum+i;// 0+1 = 1 , 1 + 2 + 3, 3+3 = 6 ,6+4 =10 ,10+5 =15
-    
+  int i = 0 ;
+  while(i<=n){
+    int j = 0;
+    while(j<=i){
+      cout<<"*"<<" ";
+      j=j+1;
+      
+    }
+  cout<<endl;
+  i = i+1;
     
   }
-  cout<<sum<<endl;//1
-
- 
-
-  
-
   
 }
 
