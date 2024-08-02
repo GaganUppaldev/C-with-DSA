9862ab8e21839314d6d599b60e52667f93837c58
GaganUppaldev <149155501+GaganUppaldev@users.noreply.github.com>
2024-08-02 16:11:49 +0530

    Create pattren Questions.cpp
    defined protocols of this file and added question 1

---
+++ pattren Questions.cpp
@@ -0,0 +1,11 @@
+//so i will paste code with question above here and try to make this less complicated , most solutions will be inside comment so you can remove that try that code
+
+//<QUESTION 1> 
+//1,2,3,4 row  like this 
+/*1,2,3,4
+  1,2,3,4
+  1,2,3,4
+  1,2,3,4
+*/
+
+//Answer 
