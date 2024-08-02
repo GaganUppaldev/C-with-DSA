3cc312baa4aaa88e210cff154d838626c88cb35a
GaganUppaldev <149155501+GaganUppaldev@users.noreply.github.com>
2024-08-03 00:19:25 +0530

    Create for loop.cpp
    Added for loop concept here and learned it also

---
+++ for loop.cpp
@@ -0,0 +1,16 @@
+//for loop
+#include <iostream>
+using namespace std;
+int main(){
+  int a ;
+  for (a=1;a<=5;a++){
+    cout<<a<<endl;
+  }
+  int b;
+  int n;
+  cin>>n;
+  cout<<n<<" is your output"<<endl;
+  for(b=1;b<=n;b++){
+    cout<<b<<" ";
+  }
+}
