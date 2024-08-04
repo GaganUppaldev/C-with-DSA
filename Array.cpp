da532c5aaeca1460d4fa027efa63800b850d7f43
GaganUppaldev <149155501+GaganUppaldev@users.noreply.github.com>
2024-08-05 01:09:51 +0530

    Update Array.cpp
    baiscs of array

--- Array.cpp
+++ Array.cpp
@@ -1,1 +1,16 @@
+//Array 
+// Baisc array creation
+#include <iostream>
+using namespace std;
+int main() {
+  int array [15];
+  cout<<array<<endl;//on printing array you will get memory address 0x7ffe7e45d100 not elememts of array
+
+  int array2 [5] = {1,2,3,4,5};
+  cout<<array2<<endl;
+  cout<<array2[0]<<endl;//we got 1 
+  //index inside array starts from 0 
+  
+  
+}
 
