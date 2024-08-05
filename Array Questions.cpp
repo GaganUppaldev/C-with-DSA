202b0049572a9bae1b6103dabaf710476b9fd938
GaganUppaldev <149155501+GaganUppaldev@users.noreply.github.com>
2024-08-05 19:13:15 +0530

    Update Array Questions.cpp
    Added reverse array method and also mentioned mistakes that i did in first attempt

--- Array Questions.cpp
+++ Array Questions.cpp
@@ -39,3 +39,44 @@ int main(){
   cout<<end<<endl;
   
 }
+//REVERSED AN ARRAY 
+/*
+#include <iostream>
+using namespace std;
+
+
+void reverse(int arr[], int size ){ //i suffered errror because i used int instead of void
+    int first = 0; //error here came because i define arr[0] = first
+    int last = size - 1;//error here came because i defined arr[size-1] = last;
+
+    while(first < last ){
+        int temp = arr[first];
+        arr[first] = arr[last];
+        arr[last] = temp;
+        first++;//i forgot to increment first
+        last--;//i forgot to deincrement last
+    }
+}
+    
+
+    
+
+void print(int arr[] , int size){//i suffered errror because i used int instead of void
+    for(int i=0;i<size;i++){
+        cout << arr[i]<<" ";
+    }
+    
+}
+
+int main(){
+ int arr [6] = {3,1,2,7,11,8};
+ int size =  sizeof(arr)/sizeof(int);
+ cout<<size<<endl;
+
+reverse(arr,size);
+print(arr,size);
+
+
+
+}
+*/
