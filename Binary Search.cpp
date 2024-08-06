47cdc56d5d86016a1a0040be9536105850265d48
GaganUppaldev <149155501+GaganUppaldev@users.noreply.github.com>
2024-08-06 17:50:38 +0530

    Update Binary Search.cpp
    Added details or concept of binary search

--- Binary Search.cpp
+++ Binary Search.cpp
@@ -1,1 +1,6 @@
 // Here i will add binary serach basics with examples and high level questions will be in Binary Search Questions.cpp file 
+
+//FIRST CONDITION FOR FOR BINARY SEARCH IS THAT ELEMENTS INSIDE AN ARRAY SHOULD BE IN FORM OF  MONOTONIC FUNCTIONS
+//Binary search  alogritum work in pattren  
+//Mid is very important and all algo is based on it we find it using  (starting - ending /2) of index 
+// provide key ( element you want to search ) ----> mid ----> compare key with mid and move forward or backword after comapring angian demans mid and again compare until it get element or try evry possiblity
