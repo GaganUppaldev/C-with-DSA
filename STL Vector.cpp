255014f63a598c32857493225ffb9b6f90fd8216
GaganUppaldev <149155501+GaganUppaldev@users.noreply.github.com>
2024-08-10 23:30:54 +0530

    Update STL Vector.cpp
    Added way to element at particular position ,element in front, element in back , printed elements inside vector using for loop and also used v.pop_back() to remove an element

--- STL Vector.cpp
+++ STL Vector.cpp
@@ -15,6 +15,25 @@ int main () {
     v.push_back(3);
     cout<<"capacity -->"<<v.capacity()<<endl;//capcity doubles
 
+    cout<<"element at 2 index "<<v.at(2)<<endl;
+
+    cout<<"element in front"<<v.front()<<endl;
+    cout<<"element in back"<<v.back()<<endl;
+
+
+    cout<<"before pop"<<endl;
+    for(int i:v){
+        cout<<i<<" ";
+    }
+
+    cout<<endl;
+
+    v.pop_back();
+    cout<<"after pop"<<endl;
+    for(int i:v){
+        cout<<i<<" ";
+    }
+
     
 
     
