3f32ef7b34cc1d50d591ad0efb0acdc8739f927a
GaganUppaldev <149155501+GaganUppaldev@users.noreply.github.com>
2024-08-10 23:54:10 +0530

    Create STL Deque.cpp
    what is   Deque ? answered in  first comment

---
+++ STL Deque.cpp
@@ -0,0 +1,5 @@
+/*  what is Deque?
+deque (short for double-ended queue) is a sequence container in the Standard Template Library (STL) that allows fast insertion and deletionof elements at both the front and the back.
+Unlike vector, which only allows efficient operations at the back, deque provides efficient operations at both ends, making it versatile for use cases where elements need to be added or 
+removed from either end of the sequence. The deque is implemented as a dynamic array of arrays, allowing it to grow and shrink efficiently from both ends.
+*/
