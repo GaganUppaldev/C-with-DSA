6bfdca5613bfaef01f5a5df7b1241ccb3e333171
GaganUppaldev <149155501+GaganUppaldev@users.noreply.github.com>
2024-08-10 23:58:31 +0530

    Update STL Deque.cpp
    Basic deque  code added

--- STL Deque.cpp
+++ STL Deque.cpp
@@ -3,3 +3,26 @@ deque (short for double-ended queue) is a sequence container in the Standard Tem
 Unlike vector, which only allows efficient operations at the back, deque provides efficient operations at both ends, making it versatile for use cases where elements need to be added or 
 removed from either end of the sequence. The deque is implemented as a dynamic array of arrays, allowing it to grow and shrink efficiently from both ends.
 */
+
+#include <iostream>
+#include <deque>
+
+using namespace std;
+int main () {
+
+    deque<int> d;
+
+    d.push_front(1);
+    d.push_back(2);
+
+    for(int i :d){
+        cout<<i<<" ";
+    }
+    
+
+    
+
+    
+    
+    
+}
