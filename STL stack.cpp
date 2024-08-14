a8033c974c1abbf1ceaf3e5f571ed441f4a16d1a
GaganUppaldev <149155501+GaganUppaldev@users.noreply.github.com>
2024-08-14 23:33:09 +0530

    Update STL stack.cpp
    Basic added and elaborated

--- STL stack.cpp
+++ STL stack.cpp
@@ -1,3 +1,16 @@
 //Stack 
 //We can learn about it with Basic example of plates on table , plate 1 first and  than plate2 on plate 1 but we will pick plate2 first than plate 1 .
 //Means First will go out in last and last will be out in first 
+
+#include<iostream>
+#include <stack>
+using namespace std;
+
+int main() {
+    stack<string>s;
+    s.push("Ideas");//first
+    s.push("are");
+    s.push("immortal");//last
+
+    cout<<s.top()<<endl; //immortal is printed and note that it was last
+}
