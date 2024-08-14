2a2c7a5a8c534de4a5a31b05fd83861fdd13822c
GaganUppaldev <149155501+GaganUppaldev@users.noreply.github.com>
2024-08-14 23:48:39 +0530

    Update STL Queue.cpp
    created queue and printed it

--- STL Queue.cpp
+++ STL Queue.cpp
@@ -1,2 +1,17 @@
 //Queue is just opposite of stack and more simple
 //Just remember first in - first out 
+
+#include<iostream>
+#include <queue>
+using namespace std;
+
+int main() {
+    queue<string>s;
+    s.push("Ideas");//first
+    s.push("are");
+    s.push("immortal");//last
+
+    cout<<s.front()<<endl;//ideas on front will be printed
+
+}
+
