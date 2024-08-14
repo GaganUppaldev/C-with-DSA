454321973c3012cd1fd0818b4a4d911d977e8c16
GaganUppaldev <149155501+GaganUppaldev@users.noreply.github.com>
2024-08-15 01:37:30 +0530

    Update Recursion.cpp
    Find number of ways to climb nth stairs problem added

--- Recursion.cpp
+++ Recursion.cpp
@@ -61,4 +61,39 @@ int main() {
     return 0;
 }
 
+//======================================================================================================================================
+//Stairs problems 
+//calculate number of ways
+
+/*
+#include<iostream>
+#include <queue>
+using namespace std;
+
+int ways(int n){
+    //Base case
+    if(n==0 || n==1){
+        return 1;
+    }
+
+    int final = ways(n-1) + ways(n-2);
+
+    return final;
+    
+}
+
+int main() {
+
+    cout<<"stairs problem"<<endl;
+    cout<<"provide number of stairs"<<endl;
+    int n ;
+    cin>>n;
+    int end = ways(n);
+    cout<<end<<endl;
+ 
+    
+
+}
+*/
+
     
