49f5b60f1b637cc2e1a796a3c60d538d0dcf9429
GaganUppaldev <149155501+GaganUppaldev@users.noreply.github.com>
2024-08-14 23:27:34 +0530

    Create STL stack.cpp
    Basic example to understand its working

---
+++ STL stack.cpp
@@ -0,0 +1,3 @@
+//Stack 
+//We can learn about it with Basic example of plates on table , plate 1 first and  than plate2 on plate 1 but we will pick plate2 first than plate 1 .
+//Means First will go out in last and last will be out in first 
