8fe47a811740d884eba58a3ee11280872dd5d1a4
GaganUppaldev <149155501+GaganUppaldev@users.noreply.github.com>
2024-08-15 23:54:41 +0530

    Update OOP.cpp
    Added Basic code

--- OOP.cpp
+++ OOP.cpp
@@ -1,3 +1,31 @@
 //Here we will code and learn concepts of Object oriented programming 
 //class 
 //It is user defined Datatype
+
+
+#include<iostream>
+
+using namespace std;
+
+
+class student{     //Created class
+
+public:
+int tenth;
+
+
+
+};
+
+
+
+
+int main() {
+  student marks; //created object for class students
+
+  marks.tenth = 100;
+  cout<<marks.tenth;
+
+    
+
+}
