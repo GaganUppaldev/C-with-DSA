b57a813eadf781bdbb156a999a6022bcae1d89b5
GaganUppaldev <149155501+GaganUppaldev@users.noreply.github.com>
2024-08-24 13:24:51 +0530

    Update Stack class.cpp
    created class stack , defined values , created node , function to push elements inside stack

--- Stack class.cpp
+++ Stack class.cpp
@@ -1,2 +1,42 @@
 //I have a stack file but in that we used stack library to perform  operations but here we will impliment it using class so this new file is created
 //Implementaion of stack in array
+
+#include <iostream>
+using namespace std;
+
+class stack{
+
+private:
+int* arr;
+int top;
+int maxsize;
+
+public:
+//constructor
+stack(int size){
+  arr = new int[size];
+  maxsize = size;
+  top = -1;
+}
+
+//function to add
+void push(int value){
+  if(top == maxsize - 1){
+    cout << "Stack is full" << endl;
+  }
+  else{
+    top++;
+    arr[top] = value;
+    cout << value << " pushed into the stack" << endl;
+    
+  }
+}
+
+};
+
+
+int main(){
+  cout<<"Implementation of stack using class : "<<endl;
+  
+  
+}
