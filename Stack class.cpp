b21d1fc0a1ee87be5620c883f016a4ed5f32a6b3
GaganUppaldev <149155501+GaganUppaldev@users.noreply.github.com>
2024-08-24 15:28:06 +0530

    Update Stack class.cpp
    Added value inside array and displayed

--- Stack class.cpp
+++ Stack class.cpp
@@ -7,46 +7,66 @@ using namespace std;
 class stack{
 
 private:
-int* arr;
-int top;
-int maxsize;
+    int* arr;
+    int top;
+    int maxsize;
 
 public:
-//constructor
-stack(int size){
-  arr = new int[size];
-  maxsize = size;
-  top = -1;
-}
+    //constructor
+    stack(int size){
+        arr = new int[size];
+        maxsize = size;
+        top = -1;
+    }
 
-//function to add
-void push(int value){
-  if(top == maxsize - 1){
-    cout << "Stack is full" << endl;
-  }
-  else{
-    top++;
-    arr[top] = value;
-    cout << value << " pushed into the stack" << endl;
+    //function to add
+    void push(int value){
+        if(top == maxsize - 1){
+            cout << "Stack is full" << endl;
+        }
+        else{
+            top++;
+            arr[top] = value;
+            /*cout << value << " pushed into the stack" << endl;*/
+        }
+    } 
 
-  }
-}
+    
+    void print(){
+        if(top == -1){
+            cout << "Stack is empty" << endl;
+        }else{
+            for(int i = top; i >= 0; i--){
+                cout << arr[i] << endl;
+            }
+            cout << endl;
+        }
+    }
 
-void pop(){
-  if(top == -1){
-    cout << "Stack is empty" << endl;
-  }else{
-    int popv = arr[top] ;
-    top--;
-    return popv;
-  }
-}
-
+    /* void pop(){
+        if(top == -1){
+            cout << "Stack is empty" << endl;
+        }else{
+            int popv = arr[top] ;
+            top--;
+            return popv;
+        }
+    } */
 };
 
-
 int main(){
-  cout<<"Implementation of stack using class : "<<endl;
+    cout << "Implementation of stack using class: " << endl;
+
+    stack arr(5);
+    arr.push(1);
+    arr.push(2);
+    arr.push(3);
+    arr.push(4);
+    arr.push(5);
+
+    arr.print();
+
+    return 0;
+} 
 
 
-}
