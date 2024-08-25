54bcea9f7dfa18e0efccdb43ca257a0857553071
GaganUppaldev <149155501+GaganUppaldev@users.noreply.github.com>
2024-08-26 02:35:13 +0530

    Update Binary tree.cpp
    Displaying value in recursive binary tree using traversal and queue

--- Binary tree.cpp
+++ Binary tree.cpp
@@ -96,6 +96,80 @@ int main(){
   
   root = buildtree(root); 
   
+//====================================================================================================================================
+//Displaying value in recursive binary tree using traversal and queue
+
+#include <iostream>
+#include<queue>
+using namespace std;
+
+class node{
+
+public:
+int data;
+node* left;
+node* right;
+
+node(int d){
+  this->data=d;
+  this->left=nullptr;
+  this->right=nullptr;
+}
+
+
+};
+
+node* buildtree(node* root){
+  cout<<"enter the data"<<endl;
+  int data;
+  cin>>data;
+  root=new node(data);
+
+  if (data == -1){
+    return nullptr;
+  }
+
+  cout<<"enter data for inserting in left of "<<data<<endl;
+  root->left=buildtree(root->left);
+
+  cout<<"enter data for inserting in right of "<<data<<endl;
+  root->right=buildtree(root->right);
+
+  return root;
+}
+
+void levelordertraversal(node* root){
+  queue<node*>q;
+  q.push(root);
+
+  while(!q.empty()){
+    node* temp = q.front();
+    cout<<temp->data<<" ";
+    q.pop();
+
+    if(temp->left){
+      q.push(temp->left);
+      
+    }
+
+    if(temp->right){
+      q.push(temp->right);
+    }
+  }
+}
+int main(){
+  node* root = nullptr;
+  
+  root = buildtree(root); 
+
+  cout<<"printing the level order traversal"<<endl;
+  levelordertraversal(root);
+
+  
+
+  return 0;
+}
+
 
   
 
