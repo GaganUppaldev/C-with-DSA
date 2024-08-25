6b45f77e78f9b4a475ae57a5789705414eaf5b7c
GaganUppaldev <149155501+GaganUppaldev@users.noreply.github.com>
2024-08-25 20:31:18 +0530

    Update Binary tree.cpp
    created basic binary tree but it can display value for now . I will add function for this later but this code is  very good to understand binary tree basic with knowledge till now i have

--- Binary tree.cpp
+++ Binary tree.cpp
@@ -1,1 +1,41 @@
 //This File is made to add Binary tree basic to advance logic  and questions will be in other file .
+
+//Basic Binary tree with current knowledge
+#include <iostream>
+using namespace std;
+
+class node{
+
+public:
+int data;
+node* left;
+node* right;
+
+node(int key){
+  data = key;
+  left = nullptr;
+  right = nullptr;
+
+}
+
+
+
+};
+
+
+
+int main(){
+
+  node* first = new node(10);
+  node* second = new node(20);
+  node* third = new node(30);
+
+  node* forth = new node(40);
+
+  //linking nodes
+  first->left=second;
+  first->right=third;
+
+  second->left=forth;
+  return 0 ;
+}
