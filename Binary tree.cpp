37b2dad010653423a06e80a3c9e09c21f294ba93
GaganUppaldev <149155501+GaganUppaldev@users.noreply.github.com>
2024-08-26 02:16:20 +0530

    Update Binary tree.cpp
    Recursive Binary Tree Creation

--- Binary tree.cpp
+++ Binary tree.cpp
@@ -43,9 +43,63 @@ int main(){
   //linking nodes
   first->left=second;
   first->right=third;
+  
 
   second->left=forth;
   
 
   preordertraversal(first);
 }
+
+//======================================================================================================================================
+//Recursive Binary Tree Creation 
+//it make Binary tree by taking inputs and use recursion 
+#include <iostream>
+using namespace std;
+
+class node{
+
+public:
+int data;
+node* left;
+node* right;
+
+node(int d){
+  this->data=d;
+  this->left=nullptr;
+  this->right=nullptr;
+}
+
+
+};
+
+node* buildtree(node* root){
+  cout<<"enter the data"<<endl;
+  int data;
+  cin>>data;
+  root=new node(data);
+
+  if (data == -1){
+    return nullptr;
+  }
+
+  cout<<"enter data for inserting in left of "<<data<<endl;
+  root->left=buildtree(root->left);
+
+  cout<<"enter data for inserting in right of "<<data<<endl;
+  root->right=buildtree(root->right);
+
+  return root;
+}
+int main(){
+  node* root = nullptr;
+  
+  root = buildtree(root); 
+  
+
+  
+
+  return 0;
+}
+
+
