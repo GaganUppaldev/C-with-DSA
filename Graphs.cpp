01777458e9487443543bfc000895e729dd677a70
GaganUppaldev <149155501+GaganUppaldev@users.noreply.github.com>
2024-09-01 20:17:06 +0530

    Update Graphs.cpp
    created basic code and a unordered list also

--- Graphs.cpp
+++ Graphs.cpp
@@ -1,1 +1,17 @@
 //File created to save all baiscs of graphs for beginners
+
+#include<iostream>
+#include<map>
+#include<unordered_map>
+#include<list>
+using namespace std;
+
+class graph{
+public:
+   unordered_map<int , list<int>>adj; //hashmap created
+
+
+};
+int main(){
+  cout<<"Graphs"<<endl;
+}
