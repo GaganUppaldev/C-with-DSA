90eb60b7186e3002868e9db1bb52925d083d96f9
GaganUppaldev <149155501+GaganUppaldev@users.noreply.github.com>
2024-09-11 10:34:58 +0530

    Update Strings.cpp
    getline added

--- Strings.cpp
+++ Strings.cpp
@@ -11,8 +11,28 @@ int main(){
     cout << name[0] <<endl;
     cout << name[2] <<endl;
     cout << "subtr = " << name.substr(0,5) <<endl;
+
     
     
 
     
+}
+
+//getline used to take input of lines with spaces  in words into string
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    
+
+    // Full line input
+    string fullLine;
+    cout << "Enter a full line: ";
+    getline(cin, fullLine); // Reads until the newline character
+    cout << "You entered: " << fullLine << endl;
+
+    return 0;
 }
