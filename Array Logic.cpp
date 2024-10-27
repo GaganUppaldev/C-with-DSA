c202341a4c9910ab3b8553f597958e9cd199c51c
Gagan Uppal <149155501+GaganUppaldev@users.noreply.github.com>
2024-10-27 23:55:48 +0530

    Update Array Logic.cpp
    Solving prime number problem

--- Array Logic.cpp
+++ Array Logic.cpp
@@ -120,3 +120,33 @@ int main() {
 
     return 0;
 }*/
+//======================================================================================================================
+//Number is prime or not
+/*
+#include <iostream>
+#include <cmath>
+
+bool isPrime(int n) {
+    if (n <= 1) return false;
+    if (n == 2) return true;
+    
+    for (int i = 2; i <= sqrt(n); i++) {
+        if (n % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main() {
+    int number = 29;
+    if (isPrime(number))
+        std::cout << number << " is a prime number.";
+    else
+        std::cout << number << " is not a prime number.";
+    
+    return 0;
+}
+
+
+*/
